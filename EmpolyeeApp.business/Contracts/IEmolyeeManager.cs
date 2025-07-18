4d7822a99ba2843149a0d7159068408027474695
RouQandel <rawanqandel62@gmail.com>
2025-07-18 14:54:23 +0300

    Add employee management interface and implementation
    Created `IEmolyeeManager` interface for employee operations, updated project to target .NET 8.0 with a reference to `EmployeeApp.DAL`, and implemented the interface in `EmployeManager` to handle employee data through the repository.

---
+++ EmpolyeeApp.business/Contracts/IEmolyeeManager.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeApp.Domain;
+
+namespace EmpolyeeApp.business.Contracts
+{
+    public interface IEmolyeeManager
+    {
+        public Task<IReadOnlyList<Employee>> GetAllEmployeesAsync();
+        public Task<Employee> CreateEmpAsync(Employee entity);
+        public Task<bool> DeleteAsync(int id);
+        public Task<Employee?> GetEmployeeIdAsync(int id);
+        public Task<Employee> UpdatedAsync(Employee entity);
+    }
+}
---
+++ EmpolyeeApp.business/Managers/EmployeManager.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeApp.DAL.Infrastructure;
+using EmployeeApp.Domain;
+using EmpolyeeApp.business.Contracts;
+
+namespace EmpolyeeApp.business.Managers
+{
+    public class EmployeManager(IEmpolyeeRepo repo) : IEmolyeeManager
+    {
+        private readonly IEmpolyeeRepo _repo = repo;
+
+        
+
+        public Task<Employee> CreateEmpAsync(Employee entity) => _repo.CreateEmpAsync(entity);
+  
+
+        public Task<bool> DeleteAsync(int id)  => _repo.DeleteAsync(id);
+      
+
+        public Task<IReadOnlyList<Employee>> GetAllEmployeesAsync() => _repo.GetAllEmployeesAsync();
+
+
+        public Task<Employee?> GetEmployeeIdAsync(int id) => _repo.GetEmployeeIdAsync(id);
+
+
+        public Task<Employee> UpdatedAsync(Employee entity) => _repo.UpdatedAsync(entity);
+
+    }
+}
