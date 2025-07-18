5251345e8bbb0941e245af0cf16958d0b41c4383
RouQandel <rawanqandel62@gmail.com>
2025-07-18 12:07:54 +0300

    Add Employee configuration and update DbContext
    Introduces `EmployeeConfiguartion` for entity configuration, defining constraints for `FirstName`, `LastName`, `Email`, and `Position`.
    
    Adds `ApplicationDbContext` class with `DbSet<Employee>` and applies configurations in `OnModelCreating`.
    
    Updates project file to target .NET 8.0 and includes necessary Entity Framework Core package references.

---
+++ EmployeeApp.DAL/Configuration/EmployeeConfiguartion.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace EmployeeApp.DAL.Configuration;
+
+public class EmployeeConfiguartion : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(e => e.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(e => e.Position)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+
+}
